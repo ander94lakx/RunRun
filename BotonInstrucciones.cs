4b09664d116c91d28780c83626a491eed2572374
ander94lakx <andergranado@outlook.com>
2014-06-06 02:03:35 +0200

    Create BotonInstrucciones.cs

---
+++ BotonInstrucciones.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class BotonInstrucciones : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnMouseDown() {
+		Camera.main.audio.Stop ();
+		audio.Play ();
+		Invoke ("CargarEscena", audio.clip.length);
+	}
+	
+	void CargarEscena() {
+		Application.LoadLevel("InstructionsScene");
+	}
+}
